eea3f745fdc49d2c447659373e77c0b5c38b24d8
Den <saloed2206@gmail.com>
2024-12-08 19:41:09 +0200

    changed path

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,16 +2,21 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
-    path: 'home',
-    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
-  },
-  {
     path: '',
     redirectTo: 'home',
     pathMatch: 'full',
   },
   {
+    path: 'home',
+    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
+  },
+  {
     path: 'product/:id',
     loadComponent: () => import('./details/details.component').then((m) => m.DetailsComponent),
+  },
+  {
+    path: '**',
+    redirectTo: 'home',
+    pathMatch: 'full',
   },
 ];
