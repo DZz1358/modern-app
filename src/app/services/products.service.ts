9065d5a5813dc3dce8faceee9c7e35d6fd3f09bc
Den <saloed2206@gmail.com>
2024-12-21 20:19:47 +0200

    added offline get product

--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -1,54 +1,63 @@
 import { HttpClient } from '@angular/common/http';
-import { inject, Injectable } from '@angular/core';
-import { delay, Observable } from 'rxjs';
+import { inject, Injectable, OnInit } from '@angular/core';
+import { from, Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { IProduct } from '../models/interface';
+import { NetworkService } from './network.service';
+import { StorageService } from '../services/storage.service';
+import { delay, map } from 'rxjs/operators';
 
-import { Geolocation } from '@capacitor/geolocation';
-import { Network } from '@capacitor/network';
-import { PluginListenerHandle } from '@capacitor/core';
 
-@Injectable({
-  providedIn: 'root'
-})
-export class ProductsService {
+
+@Injectable()
+
+export class ProductsService implements OnInit {
   http = inject(HttpClient);
-  public checkNetwork: boolean | undefined
+  networkService = inject(NetworkService);
+  storageService = inject(StorageService);
+
+  private isConnected: boolean | undefined;
 
   constructor() {
   }
 
-  observeNetworkStatus(): Observable<any> {
-    return new Observable((observer) => {
-      let listener: PluginListenerHandle;
+  ngOnInit(): void {
+  }
 
-      const setupListener = async () => {
-        const currentStatus = await Network.getStatus();
-        observer.next(currentStatus);
-
-        listener = await Network.addListener('networkStatusChange', (status) => {
-          observer.next(status);
-        });
-      };
-
-      setupListener();
-
-      return async () => {
-        if (listener) {
-          await listener.remove();
-        }
-      };
+  public subscribeToNetworkStatus(): void {
+    this.networkService.getCurrentNetworkStatus().subscribe((status) => {
+      this.isConnected = status.connected;
     });
   }
 
   getProducts(): Observable<IProduct[]> {
-    return this.http.get<IProduct[]>(`${environment.apiUrl}`).pipe(
-      delay(3000)
-    )
+
+    if (this.isConnected) {
+      return this.http.get<IProduct[]>(`${environment.apiUrl}`).pipe(
+        delay(1000)
+      )
+    } else {
+      return from(this.storageService.getFromStorage('products'));
+    }
   }
 
+
   getProduct(id: string): Observable<IProduct> {
-    return this.http.get<IProduct>(`${environment.apiUrl}/${id}`);
+    if (this.isConnected) {
+      return this.http.get<IProduct>(`${environment.apiUrl}/${id}`);
+    } else {
+      const products$ = from(this.storageService.getFromStorage('products'));
+
+      return products$.pipe(
+        map((products: IProduct[]) => {
+          const product = products.find(product => product.id === +id);
+          if (!product) {
+            throw new Error(`Product with id ${id} not found`);
+          }
+          return product;
+        })
+      );
+    }
   }
 
 }
