bec9cfbc98506a14602d5c2a79597ecab7ae2faa
Den <saloed2206@gmail.com>
2024-12-21 18:21:14 +0200

    fix bug with refresh

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,10 +4,11 @@ import { DetailsComponent } from './details/details.component';
 import { CartComponent } from './cart/cart.component';
 
 export const routes: Routes = [
-  {
-    path: '',
-    component: HomePage
-  },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  // {
+  //   path: '',
+  //   component: HomePage
+  // },
   {
     path: 'home',
     component: HomePage
